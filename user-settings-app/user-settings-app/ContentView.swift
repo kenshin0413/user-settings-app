cb654e50d49b86adcc876799c8d3cd13a55ca34b
kenshin0413 <kenshin1234569@gmail.com>
2025-07-08 13:52:23 +0900

    初期

---
+++ user-settings-app/user-settings-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  user-settings-app
+//
+//  Created by miyamotokenshin on R 7/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ user-settings-app/user-settings-app/user_settings_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  user_settings_appApp.swift
+//  user-settings-app
+//
+//  Created by miyamotokenshin on R 7/07/08.
+//
+
+import SwiftUI
+
+@main
+struct user_settings_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
