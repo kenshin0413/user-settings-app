161289a0a011dea2ebdea7dd7fffd74d20993302
kenshin0413 <kenshin1234569@gmail.com>
2025-07-09 15:32:06 +0900

    年齢設定の追加

--- user-settings-app/user-settings-app/ContentView.swift
+++ user-settings-app/user-settings-app/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State var username: String = ""
+    @State var ageCount: Int = 20
     var body: some View {
         NavigationStack {
-            VStack {
+            VStack(spacing: -10) {
                 TextField("ユーザー名を入力してください", text: $username)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
+                
+                Stepper(value: $ageCount, in: 0...100, step: 1) {
+                    Text("年齢:\(ageCount)歳")
+                }
+                .padding()
+                
                 Button {
                     UserDefaults.standard.set(username, forKey: "username")
+                    UserDefaults.standard.set(ageCount, forKey: "ageCount")
                 } label: {
                     Text("保存")
                 }
@@ -28,6 +36,7 @@ struct ContentView: View {
         // アプリ起動時の処理
         .onAppear {
             username = UserDefaults.standard.string(forKey: "username") ?? ""
+            ageCount = UserDefaults.standard.integer(forKey: "ageCount")
         }
     }
 }
